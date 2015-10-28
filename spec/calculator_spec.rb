8d786ea8464b722442d17135f391302627e5185b
Ken Fry <kfry@student.cscc.edu>
2015-10-27 21:09:32 -0400

    add multiple numbers

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -8,7 +8,16 @@ describe Calculator do
 			calc = Calculator.new
 			actual = calc.add(2,3)
 			expect(actual).to be_a Integer
+			
 		end
+	
+		it 'can add many numbers' do
+			calc = Calculator.new
+			actual = calc.add(1,2,3,4)
+			expect(actual).to eq 10
+		end
+	
+	
 	end
 
 
