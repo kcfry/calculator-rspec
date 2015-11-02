3f071ab3594353c979787beed70101cf545270df
Ken Fry <kfry@student.cscc.edu>
2015-11-02 13:37:41 -0500

    finished

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -6,6 +6,13 @@ class Calculator
 	
 	def sub(*n1)
 		n1.inject(:-)
+	end
 
+	def mult(*n1)
+		n1.inject(:*)
+	end
+		
+	def div(*n1)
+		n1.inject(:/)
 	end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -33,4 +33,32 @@ describe Calculator do
 			expect(actual). to eq 0
 		end	
 	end		
-end
+	
+	describe 'multiplication' do
+		it 'returns an integer' do
+			calc = Calculator.new
+			actual = calc.mult(2,3)
+			expect(actual).to be_a Integer
+		end
+		
+		it 'can multiply many numbers' do
+			calc = Calculator.new
+			actual = calc.mult(3,2)
+			expect(actual). to eq 6
+		end	
+	end		
+	
+	describe 'division' do
+		it 'returns an integer' do
+			calc = Calculator.new
+			actual = calc.div(2,3)
+			expect(actual).to be_a Integer
+		end
+		
+		it 'can divide many numbers' do
+			calc = Calculator.new
+			actual = calc.div(10,2)
+			expect(actual).to eq 5
+		end	
+	end	
+end	
