80a1759953337d16268ba2139fec981012a86f2b
Ken Fry <kfry@student.cscc.edu>
2015-11-03 20:05:39 -0500

    Finished for real this time...I think

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -16,8 +16,12 @@ describe Calculator do
 			actual = calc.add(1,2,3,4)
 			expect(actual).to eq 10
 		end
-	
-	
+		
+		it 'can add by 0' do
+			calc = Calculator.new
+			actual = calc.add(2,0)
+			expect(actual).to eq 2
+		end
 	end
 
 	describe 'subtraction' do
@@ -32,6 +36,19 @@ describe Calculator do
 			actual = calc.sub(3,2,1)
 			expect(actual). to eq 0
 		end	
+	
+		it 'can subtract zero' do
+			calc = Calculator.new
+			actual = calc.sub(4,0)
+			expect(actual). to eq 4
+		end
+		
+		it 'can subtract in different order' do
+			calc = Calculator.new
+			actual = calc.sub(3,5)
+			expect(actual).to eq -2
+		end
+		
 	end		
 	
 	describe 'multiplication' do
@@ -43,9 +60,22 @@ describe Calculator do
 		
 		it 'can multiply many numbers' do
 			calc = Calculator.new
-			actual = calc.mult(3,2)
+			actual = calc.mult(3,2,1)
 			expect(actual). to eq 6
 		end	
+		
+		it 'can multiply by zero' do
+			calc = Calculator.new
+			actual = calc.mult(4,0)
+			expect(actual).to eq 0
+		end
+		
+		it 'can multiply by one' do 
+			calc = Calculator.new
+			actual = calc.mult(4,1)
+			expect(actual).to eq 4
+		end
+		
 	end		
 	
 	describe 'division' do
@@ -57,8 +87,19 @@ describe Calculator do
 		
 		it 'can divide many numbers' do
 			calc = Calculator.new
-			actual = calc.div(10,2)
-			expect(actual).to eq 5
+			actual = calc.div(12,3,2)
+			expect(actual).to eq 2
+		end
+			
+		it 'can divide by one' do
+			calc = Calculator.new
+			actual = calc.div(10,1)
+			expect(actual).to eq 10
+		end
+		
+		it 'can not divide by zero' do
+			calc = Calculator.new
+			expect{calc.div(4,0)}.to raise_error ZeroDivisionError
 		end	
 	end	
 end	
